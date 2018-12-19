7118b327905fdbac6693da0860c003e0de6fe5a3
maximelat <maximelat@gmail.com>
2018-12-19 13:20:17 +0100

    test shiny

--- shiny.R
+++ shiny.R
@@ -5,7 +5,10 @@ ui <- fluidPage()
 server <- function(input, output){}
 shinyApp(ui = ui, server = server)
 library(rsconnect)
-rsconnect::deployApp('/Users/maximelat/Library/Mobile\ Documents/com\~apple\~CloudDocs/Certifications/EDX/UMR_BIOSTAT')
+rsconnect::deployApp('~CloudDocs/Certifications/EDX/UMR_BIOSTAT/shiny.R')
 
+rsconnect::setAccountInfo(name='maximelat',
+                          token='466285C3BDC7F64A8370C8751D404905',
+                          secret='+0z7k5+3dF9+ylFMFDYGGj53Rjum7/xTTIJHLdb1')
 library(shiny)
 runExample("01_hello")
